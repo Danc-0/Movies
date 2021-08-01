26317a834b96b78f49d08cc4a28779e6b66a3d51
Danc-0 <lunjaludaniel@gmail.com>
2021-08-01 14:43:09 +0300

    Hide API KEYS

--- app/src/main/java/com/example/movieapp/data/datasource/ApiHelper.kt
+++ app/src/main/java/com/example/movieapp/data/datasource/ApiHelper.kt
@@ -1,38 +1,43 @@
 package com.example.movieapp.data.datasource
 
+import android.content.res.Resources
+import com.example.movieapp.BuildConfig
+import com.example.movieapp.R
 import com.example.movieapp.api.ApiService
 import javax.inject.Inject
 
 class ApiHelper @Inject constructor(private val apiService: ApiService) {
 
+    val apiKey: String = BuildConfig.API_KEY
+
     suspend fun getMovies() = apiService.getMovieList(
-        "8ed700250305de124bef08dbb686472a",
+        apiKey,
         1
     )
 
     suspend fun getMoviesGenre() = apiService.getMovieGenreList(
-        "8ed700250305de124bef08dbb686472a",
+        apiKey,
         "en-US"
     )
 
     suspend fun getTVGenre() = apiService.getTVListGenreList(
-        "8ed700250305de124bef08dbb686472a",
+        apiKey,
         "en-US"
     )
 
     suspend fun getMovieRecommendations(movieID: Int, pageNo: Int) = apiService.getRecommendedMovieList(
         movieID,
-        "8ed700250305de124bef08dbb686472a",
+        apiKey,
         "en-US",
         pageNo
     )
 
     suspend fun getLanguageList() = apiService.getLanguageList(
-        "8ed700250305de124bef08dbb686472a"
+        apiKey
     )
 
     suspend fun getMoviesByGenre(genre_id: Int) = apiService.getMoviesListByGenre(
-        "8ed700250305de124bef08dbb686472a",
+        apiKey,
         genre_id
     )
 }
